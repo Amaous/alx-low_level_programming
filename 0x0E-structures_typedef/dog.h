7eafcabc08d8764d0a07010375be158b1a1f8e5f
Kindsew Shimelash <kingminas628@gmail.com>
2023-11-06 15:43:04 -0800

    Defining a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+  *struct - declaring with tag_name dog
+  *
+  *@name : 1st member
+  *@age : 2nd member
+  *&owner : last member
+  *
+  */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
